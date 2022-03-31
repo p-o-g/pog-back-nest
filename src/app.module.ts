06a7adb8b934065e3d200f45b47df5887e8f1f40
Simo Ryu <cloneofsimo@gmail.com>
2022-03-31 09:31:47 +0000

    feat: swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
+
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PostModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostModule, TypeOrmModule.forRoot(typeORMConfig)],
 })
 export class AppModule {}
---
+++ src/utils/swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('NestJS POG API')
+    .setDescription('API for the NestJS POG')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+}
